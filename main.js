35c80a4ac968af5073b2dca6fa0d923eb366bade
Wasiq Nazir <nwasiq@gmail.com>
2021-02-28 00:27:49 +0500

    handled LTC addr generation

--- main.js
+++ main.js
@@ -3,15 +3,28 @@ const AddressService = require('./services/AddressService')
 
 require('dotenv').config()
 
-const bip32Priv = process.env.BIP32_PRIV
-const secondaryPubKey = process.env.SECONDARY_PUB_KEY
+const btcBip32Priv = process.env.BTC_BIP32_PRIV
+const btcSecondaryPubKey = process.env.BTC_SECONDARY_PUB_KEY
 
-let defaultAddr = AddressService.generateDefaultBlockioAddress(bip32Priv, secondaryPubKey)
-let subsequentAddr = AddressService.generateSubsequentBlockioAddress(bip32Priv, secondaryPubKey, 4)
-let nativeSegwitAddr = AddressService.generateP2wshBlockioAddress(bip32Priv, secondaryPubKey, 5)
+const ltcBip32Priv = process.env.LTC_BIP32_PRIV
+const ltcSecondaryPubKey = process.env.LTC_SECONDARY_PUB_KEY
+
+// const defaultAddr = AddressService.generateDefaultBlockioAddress(btcBip32Priv, btcSecondaryPubKey, networks.BITCOIN)
+// const subsequentAddr = AddressService.generateSubsequentBlockioAddress(btcBip32Priv, btcSecondaryPubKey, 3, networks.BITCOIN)
+// const nativeSegwitAddr = AddressService.generateP2wshBlockioAddress(btcBip32Priv, btcSecondaryPubKey, 4, networks.BITCOIN)
+
+// console.log(defaultAddr)
+// console.log(subsequentAddr)
+// console.log(nativeSegwitAddr)
+
+// AddressService.checkBlockioAddressBalance(defaultAddr, networks.BITCOIN)
+
+const defaultAddr = AddressService.generateDefaultBlockioAddress(ltcBip32Priv, ltcSecondaryPubKey, networks.LITECOIN)
+const subsequentAddr = AddressService.generateSubsequentBlockioAddress(ltcBip32Priv, ltcSecondaryPubKey, 3, networks.LITECOIN)
+const nativeSegwitAddr = AddressService.generateP2wshBlockioAddress(ltcBip32Priv, ltcSecondaryPubKey, 4, networks.LITECOIN)
 
 console.log(defaultAddr)
 console.log(subsequentAddr)
 console.log(nativeSegwitAddr)
 
-AddressService.checkBlockioAddressBalance(defaultAddr, networks.BITCOIN)
+AddressService.checkBlockioAddressBalance(defaultAddr, networks.LITECOIN)
--- services/AddressService.js
+++ services/AddressService.js
@@ -1,14 +1,50 @@
 const bitcoin = require('bitcoinjs-lib')
 const fetch = require('node-fetch')
+const networks = require('../networks')
 
 const sochainApiUrl = 'https://sochain.com/api/v2/'
 const getAddrApi = 'get_address_balance/'
 
+const LITECOIN = {
+  messagePrefix: '\x19Litecoin Signed Message:\n',
+  bech32: 'ltc',
+  bip32: {
+    public: 0x019da462,
+    private: 0x019d9cfe
+  },
+  pubKeyHash: 0x30,
+  scriptHash: 0x32,
+  wif: 0xb0
+}
+
+const BITCOIN = {
+  messagePrefix: '\x18Bitcoin Signed Message:\n',
+  bech32: 'bc',
+  bip32: {
+    public: 0x0488b21e,
+    private: 0x0488ade4
+  },
+  pubKeyHash: 0x00,
+  scriptHash: 0x05,
+  wif: 0x80
+}
+
 const AddressService = function () {}
 
 // generate a P2SH, pay-to-multisig (2-of-2) address
-AddressService.prototype.generateDefaultBlockioAddress = (bip32PrivKey, secondaryPubKey) => {
-  const PUB1 = bitcoin.bip32.fromBase58(bip32PrivKey).derivePath('m/0/0').publicKey
+AddressService.prototype.generateDefaultBlockioAddress = (bip32PrivKey, secondaryPubKey, crypto) => {
+  let network
+
+  switch (crypto) {
+    case networks.BITCOIN:
+      network = BITCOIN
+      break
+    case networks.LITECOIN:
+      network = LITECOIN
+      break
+  }
+
+  const PUB1 = bitcoin.bip32.fromBase58(bip32PrivKey, network).derivePath('m/0/0').publicKey
   const PUB2 = Buffer.from(secondaryPubKey, 'hex')
 
   const pubkeys = [PUB1, PUB2]
@@ -19,8 +55,19 @@ AddressService.prototype.generateDefaultBlockioAddress = (bip32PrivKey, secondar
 }
 
 // generate a P2SH(P2WSH(...)), pay-to-multisig (2-of-2) address
-AddressService.prototype.generateSubsequentBlockioAddress = (bip32PrivKey, secondaryPubKey, i) => {
-  const PUB1 = bitcoin.bip32.fromBase58(bip32PrivKey).derivePath('m/' + i + '/0').publicKey
+AddressService.prototype.generateSubsequentBlockioAddress = (bip32PrivKey, secondaryPubKey, i, crypto) => {
+  let network
+
+  switch (crypto) {
+    case networks.BITCOIN:
+      network = BITCOIN
+      break
+    case networks.LITECOIN:
+      network = LITECOIN
+      break
+  }
+
+  const PUB1 = bitcoin.bip32.fromBase58(bip32PrivKey, network).derivePath('m/' + i + '/0').publicKey
   const PUB2 = Buffer.from(secondaryPubKey, 'hex')
 
   const pubkeys = [PUB1, PUB2]
@@ -33,13 +80,23 @@ AddressService.prototype.generateSubsequentBlockioAddress = (bip32PrivKey, secon
 }
 
 // generate a P2WSH (SegWit), pay-to-multisig (2-of-2) address
-AddressService.prototype.generateP2wshBlockioAddress = (bip32PrivKey, secondaryPubKey, i) => {
-  const PUB1 = bitcoin.bip32.fromBase58(bip32PrivKey).derivePath('m/' + i + '/0').publicKey
+AddressService.prototype.generateP2wshBlockioAddress = (bip32PrivKey, secondaryPubKey, i, crypto) => {
+  let network
+
+  switch (crypto) {
+    case networks.BITCOIN:
+      network = BITCOIN
+      break
+    case networks.LITECOIN:
+      network = LITECOIN
+      break
+  }
+  const PUB1 = bitcoin.bip32.fromBase58(bip32PrivKey, network).derivePath('m/' + i + '/0').publicKey
   const PUB2 = Buffer.from(secondaryPubKey, 'hex')
 
   const pubkeys = [PUB1, PUB2]
   const address = bitcoin.payments.p2wsh({
-    redeem: bitcoin.payments.p2ms({ m: 2, pubkeys })
+    redeem: bitcoin.payments.p2ms({ m: 2, pubkeys, network: network })
   })
   return address.address
 }
