0ace1c108b114186848de8fc7d01a53430531b81
Wasiq Nazir <nwasiq@gmail.com>
2021-03-31 03:14:51 +0500

    fixed up example

--- example.js
+++ example.js
@@ -1,5 +1,4 @@
 const Sweeper = require('./src/sweeper')
-require('dotenv').config()
 
 const n = process.env.N
 const bip32 = process.env.PRIVATE_KEY1_BIP32
@@ -7,6 +6,11 @@ const privkey2 = process.env.PRIVATE_KEY2
 const toAddr = process.env.DESTINATION_ADDR
 const network = process.env.NETWORK
 const derivationPath = process.env.DERIVATION_PATH
+
+if (!n || !bip32 || !privkey2 || !toAddr || !network || !derivationPath) {
+  console.log('One or more required arguments are missing')
+  process.exit(0)
+}
 
 const sweep = new Sweeper(network, bip32, privkey2, toAddr, n, derivationPath)
 
